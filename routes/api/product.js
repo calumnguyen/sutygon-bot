501e9c71d10c2658bd8fe04f636b8b7a03c5aa02
rameezamemon1 <rameeza.memon1@gmail.com>
2020-11-24 02:54:38 -0800

    Update product.js

--- routes/api/product.js
+++ routes/api/product.js
@@ -4,380 +4,359 @@ const auth = require("../../middleware/auth");
 const Product = require("../../models/Product");
 const { check, validationResult } = require("express-validator");
 var multer = require('multer')
-var upload = multer({ dest: 'client/public/uploads/products' })
-const FILE_PATH = 'client/public/uploads/products';
 var cloudinary = require('cloudinary')
 const config = require("config");
 
 // cloundinary configuration
 cloudinary.config({
-    cloud_name: config.get("cloud_name"),
-    api_key: config.get("api_key"),
-    api_secret: config.get("api_secret")
+  cloud_name: config.get("cloud_name"),
+  api_key: config.get("api_key"),
+  api_secret: config.get("api_secret")
 });
 
 // multer configuration
 var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, FILE_PATH)
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname.split(' ').join('_'))
+  filename: function(req, file, callback) {
+    callback(null, Date.now() + file.originalname);
     }
-})
-var upload = multer({ storage: storage })
+ 
+});
+const imageFilter = function(req, file, cb) {
+  // accept image files only
+  if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
+  return cb(new Error("Only image files are accepted!"), false);
+  }
+  cb(null, true);
+  };
 
-router.post('/myroute', (req, res) => {
-  res.send('response from the server');
-})
+var upload = multer({ storage: storage, fileFilter: imageFilter });
+
+
 // @route   POST api/products/add
 // @desc    Add New Product
 // @access  private
 router.post(
-    "/add",
-    [
-        check("name", "Product Name Required").not().isEmpty(),
-        check('image', "Product Image Required").not().isEmpty(),
-        check("color", "Product Color Required").isArray().not().isEmpty(),
-    ],
-    auth,
-    upload.single('image'),
-    async (req, res) => {
-       const body = JSON.parse(JSON.stringify(req.body)); // req.body = [Object: null prototype] { title: 'product' }
-        const image = req.file.path
-        try {
+  "/add",
+  [
+    check("name", "Product Name Required").not().isEmpty(),
+    check('image', "Product Image Required").not().isEmpty(),
+    check("color", "Product Color Required").isArray().not().isEmpty(),
+  ],
+  auth,
+  upload.single('image'),
+  async (req, res) => {
+    const body = JSON.parse(JSON.stringify(req.body)); // req.body = [Object: null prototype] { title: 'product' }
+    const image = req.file.path
+    console.log("req.path",req.path)
+    try {
 
-          cloudinary.uploader.upload(image, async function(result) {
-            const productBody = {
-                name: body.name,
-                productId: body.productId,
-                tags: body.tags,
-                image: result.secure_url,
-                color: JSON.parse(req.body.color),
-            };
-            let product = new Product(productBody);
-            await product.save();
-            res.json({ product, msg: "Product Added Successfully" });
-          })
+      cloudinary.uploader.upload(image, async function (result) {
+        const productBody = {
+          name: body.name,
+          productId: body.productId,
+          tags: body.tags,
+          image: result.secure_url,
+          color: JSON.parse(req.body.color),
+        };
+        let product = new Product(productBody);
+        await product.save();
+        res.json({ product, msg: "Product Added Successfully" });
+      })
 
 
-       } catch (err) {
-            console.log(err);
-            res
-                .status(500)
-                .send("Server error");
-        }
+    } catch (err) {
+      console.log(err);
+      res
+        .status(500)
+        .send("Server error");
     }
+  }
 );
-
-
 
 // @route  POST api/products/barcode_update/:id
 // @desc   Update a Product for Barcode
 // @access Private
-router.post(
-    "/barcode_update/:id",
-    auth,
-    async (req, res) => {
-        try {
-            const body = JSON.parse(JSON.stringify(req.body)); // req.body = [Object: null prototype] { title: 'product' }
+router.post("/barcode_update/:id", auth, async (req, res) => {
+  try {
+    const body = JSON.parse(JSON.stringify(req.body)); // req.body = [Object: null prototype] { title: 'product' }
 
-            await Product.updateOne({ _id: req.params.id }, {
-                $set: {
-                    color: body.color,
-                }
-            });
-            res
-                .json({ msg: "Product Updated Successfully" });
-        } catch (err) {
-            console.error(err.message);
-            res
-                .status(500)
-                .json({ errors: [{ msg: "Server Error: Something went wrong" }] });
-        }
-    }
-);
+    await Product.updateOne(
+      { _id: req.params.id },
+      {
+        $set: {
+          color: body.color,
+        },
+      }
+    );
+    res.json({ msg: "Product Updated Successfully" });
+  } catch (err) {
+    console.error(err.message);
+    res
+      .status(500)
+      .json({ errors: [{ msg: "Server Error: Something went wrong" }] });
+  }
+});
 // @route  POST api/products/index_update/:id
 // @desc   Update a Product after renting
 // @access Private
-router.post(
-    "/index_update/:id",
-    auth,
-    async (req, res) => {
-        try {
-            const body = JSON.parse(JSON.stringify(req.body)); // req.body = [Object: null prototype] { title: 'product' }
-            await Product.updateOne({ _id: req.params.id }, {
-                $set: {
-                    color: body.color,
-                }
-            });
-            res
-                .json({ msg: "Product Updated Successfully" });
-        } catch (err) {
-            console.error(err.message);
-            res
-                .status(500)
-                .json({ errors: [{ msg: "Server Error: Something went wrong" }] });
-        }
-    }
-);
+router.post("/index_update/:id", auth, async (req, res) => {
+  try {
+    const body = JSON.parse(JSON.stringify(req.body)); // req.body = [Object: null prototype] { title: 'product' }
+    await Product.updateOne(
+      { _id: req.params.id },
+      {
+        $set: {
+          color: body.color,
+        },
+      }
+    );
+    res.json({ msg: "Product Updated Successfully" });
+  } catch (err) {
+    console.error(err.message);
+    res
+      .status(500)
+      .json({ errors: [{ msg: "Server Error: Something went wrong" }] });
+  }
+});
 
 // @route  POST api/products/changeStatus/:id
 // @desc   changeStatus
 // @access Private
-router.post(
-    "/changeStatus/:id/:status",
-    auth,
-    async (req, res) => {
-        try {
-            await Product.updateOne({ _id: req.params.id }, {
-                $set: {
-                    disabled: req.params.status,
-                }
-            });
-            res.json({ msg: "Product Status changed Successfully" });
-        } catch (err) {
-            console.error(err.message);
-            res
-                .status(500)
-                .json({ errors: [{ msg: "Server Error: Something went wrong" }] });
-        }
-    }
-);
+router.post("/changeStatus/:id/:status", auth, async (req, res) => {
+  try {
+    await Product.updateOne(
+      { _id: req.params.id },
+      {
+        $set: {
+          disabled: req.params.status,
+        },
+      }
+    );
+    res.json({ msg: "Product Status changed Successfully" });
+  } catch (err) {
+    console.error(err.message);
+    res
+      .status(500)
+      .json({ errors: [{ msg: "Server Error: Something went wrong" }] });
+  }
+});
 
 // @route  POST api/products/item_delete/:id
 // @desc   Update a Product to Delete Item
 // @access Private
-router.post(
-    "/item_delete/:id",
-    auth,
-    async (req, res) => {
-        try {
-            const body = JSON.parse(JSON.stringify(req.body)); // req.body = [Object: null prototype] { title: 'product' }
-            const product = await Product.updateOne({ _id: req.params.id }, {
-                $set: {
-                    color: body.color,
-                }
-            });
-            res
-                .json({ msg: "Item Deleted Successfully" });
-        } catch (err) {
-            console.error(err.message);
-            res
-                .status(500)
-                .json({ errors: [{ msg: "Server Error: Something went wrong" }] });
-        }
-    }
-);
+router.post("/item_delete/:id", auth, async (req, res) => {
+  try {
+    const body = JSON.parse(JSON.stringify(req.body)); // req.body = [Object: null prototype] { title: 'product' }
+    const product = await Product.updateOne(
+      { _id: req.params.id },
+      {
+        $set: {
+          color: body.color,
+        },
+      }
+    );
+    res.json({ msg: "Item Deleted Successfully" });
+  } catch (err) {
+    console.error(err.message);
+    res
+      .status(500)
+      .json({ errors: [{ msg: "Server Error: Something went wrong" }] });
+  }
+});
 
 // @route  POST api/products/:id
 // @desc   Update a Product
 // @access Private
-router.post(
-    "/:id",
-    auth,
-    upload.single('image'),
-    async (req, res) => {
-        try {
-            let updatedImage = "";
-
-            const body = JSON.parse(JSON.stringify(req.body)); // req.body = [Object: null prototype] { title: 'product' }
-            if (req.file === undefined) {
-                updatedImage = body.image;
+router.post("/:id",
+  auth,
+  upload.single('image'),
+  async (req, res) => {
+    try {
+      const body = JSON.parse(JSON.stringify(req.body)); // req.body = [Object: null prototype] { title: 'product' }
+    
+      if (req.file === undefined) {
+        await Product.updateOne(
+          { _id: req.params.id },
+          {
+            $set: {
+              name: body.name,
+              tags: body.tags,
+              image:  body.image,
+              color: JSON.parse(body.color),
+            },
+          }
+        );
+        res.json({ msg: "Product Updated Successfully" });
+      } else {
+        const image = req.file.path
+        cloudinary.uploader.upload(image, async function (result) {
+          console.log(result)
+          await Product.updateOne(
+            { _id: req.params.id },
+            {
+              $set: {
+                name: body.name,
+                tags: body.tags,
+                image: result.secure_url,
+                color: JSON.parse(body.color),
+              },
             }
-            else {
-                updatedImage = `/uploads/products/${req.file.originalname}`;
-            }
-            await Product.updateOne({ _id: req.params.id }, {
-                $set: {
-                    name: body.name,
-                    tags: body.tags,
-                    image: updatedImage,
-                    color: JSON.parse(body.color),
-                }
-            });
-            res
-                .json({ msg: "Product Updated Successfully" });
-        } catch (err) {
-            console.error(err.message);
-            res
-                .status(500)
-                .json({ errors: [{ msg: "Server Error: Something went wrong" }] });
-        }
+          );
+          res.json({ msg: "Product Updated Successfully" });
+      })
+      }
+    } catch (err) {
+      console.error(err.message);
+      res
+        .status(500)
+        .json({ errors: [{ msg: "Server Error: Something went wrong" }] });
     }
-);
+  });
 
 // @route   GET api/products
 // @desc    Get all products
 // @access  Private
 router.get("/", auth,
 
-    async (req, res) => {
-        try {
-            const products = await Product.find().sort({ 'date' : -1 });
-            res
-                .status(200)
-                .json(products);
-        } catch (err) {
-            console.log(err);
-            res
-                .status(500)
-                .send("Server Error!");
-        }
-    });
+  async (req, res) => {
+    try {
+      const products = await Product.find().sort({ 'date': -1 });
+      res
+        .status(200)
+        .json(products);
+    } catch (err) {
+      console.log(err);
+      res
+        .status(500)
+        .send("Server Error!");
+    }
+  });
 
 
 // @route   GET api/products/search/search val
 // @desc    Search products
 // @access  Private
-router.get("/search/:val", auth,
+router.get(
+  "/search/:val",
+  auth,
 
-    async (req, res) => {
-        try {
-            const search = req.params.val;
-            const products = await Product.find({
-                $or: [
-                    { name: search },
-                    { color: search },
-                    { size: search },
-                    { fabric: search },
-                    { availableQuantity: search },
-                    { rentedQuantity: search },
-                ]
-            });
-            res
-                .status(200)
-                .json(products);
-        } catch (err) {
-            console.log(err);
-            res
-                .status(500)
-                .send("Server Error!");
-        }
-    });
+  async (req, res) => {
+    try {
+      const search = req.params.val;
+      const products = await Product.find({
+        $or: [
+          { name: search },
+          { color: search },
+          { size: search },
+          { fabric: search },
+          { availableQuantity: search },
+          { rentedQuantity: search },
+        ],
+      });
+      res.status(200).json(products);
+    } catch (err) {
+      console.log(err);
+      res.status(500).send("Server Error!");
+    }
+  }
+);
 
 // @route  GET api/products/:id
 // @desc   Get Product by id
 // @access Private
-router.get("/:id", auth,
-    async (req, res) => {
-        try {
-            const product = await Product.findById(req.params.id);
+router.get("/:id", auth, async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
 
-            if (!product) {
-                return res
-                    .status(404)
-                    .json({ msg: "No Product found" });
-            }
+    if (!product) {
+      return res.status(404).json({ msg: "No Product found" });
+    }
 
-            res.json(product);
-        } catch (err) {
-            console.error(err.message);
-            // Check if id is not valid
-            if (err.kind === "ObjectId") {
-                return res
-                    .status(404)
-                    .json({ msg: "No Product found" });
-            }
-            res
-                .status(500)
-                .json({ errors: [{ msg: "Server Error: Something went wrong" }] });
-        }
-    });
-
-
+    res.json(product);
+  } catch (err) {
+    console.error(err.message);
+    // Check if id is not valid
+    if (err.kind === "ObjectId") {
+      return res.status(404).json({ msg: "No Product found" });
+    }
+    res
+      .status(500)
+      .json({ errors: [{ msg: "Server Error: Something went wrong" }] });
+  }
+});
 
 // @route  GET api/products/:name
 // @desc   Get Product (Search for product by name)
 // @access Private
-router.get("/:name",
-    auth,
-    async (req, res) => {
-        try {
-            const product = await Product.findOne({ name: { $eq: req.params.name } });
+router.get("/:name", auth, async (req, res) => {
+  try {
+    const product = await Product.findOne({ name: { $eq: req.params.name } });
 
-            if (!product) {
-                return res
-                    .status(404)
-                    .json({ msg: "No Products found" });
-            }
-            res
-                .status(200)
-                .json(product);
-
-        } catch (err) {
-            console.error(err.message);
-            // Check if id is not valid
-            if (err.kind === "ObjectId") {
-                return res
-                    .status(404)
-                    .json({ msg: "No Product found" });
-            }
-            res
-                .status(500)
-                .json({ errors: [{ msg: "Server Error: Something went wrong" }] });
-        }
-    });
+    if (!product) {
+      return res.status(404).json({ msg: "No Products found" });
+    }
+    res.status(200).json(product);
+  } catch (err) {
+    console.error(err.message);
+    // Check if id is not valid
+    if (err.kind === "ObjectId") {
+      return res.status(404).json({ msg: "No Product found" });
+    }
+    res
+      .status(500)
+      .json({ errors: [{ msg: "Server Error: Something went wrong" }] });
+  }
+});
 
 // @route  DELETE api/products/:id
 // @desc   Delete a Product
 // @access Private
-router.delete("/:id",
+router.delete(
+  "/:id",
 
-    async (req, res) => {
-        try {
-            const product = await Product.findById(req.params.id);
+  async (req, res) => {
+    try {
+      const product = await Product.findById(req.params.id);
 
-            if (!product) {
-                return res
-                    .status(404)
-                    .json({ msg: "No Product found" });
-            }
+      if (!product) {
+        return res.status(404).json({ msg: "No Product found" });
+      }
 
-            await product.remove();
+      await product.remove();
 
-            res
-                .status(200)
-                .json({ msg: "Product Successfully Removed" });
-        } catch (err) {
-            console.error(err.message);
-            if (err.kind === "ObjectId") {
-                return res
-                    .status(404)
-                    .json({ msg: "No Product found" });
-            }
-            res
-                .status(500)
-                .json({ errors: [{ msg: "Server Error: Something went wrong" }] });
-        }
-    });
-
-
+      res.status(200).json({ msg: "Product Successfully Removed" });
+    } catch (err) {
+      console.error(err.message);
+      if (err.kind === "ObjectId") {
+        return res.status(404).json({ msg: "No Product found" });
+      }
+      res
+        .status(500)
+        .json({ errors: [{ msg: "Server Error: Something went wrong" }] });
+    }
+  }
+);
 
 // @route   GET api/products/search/search val
 // @desc    Search products
 // @access  Private
-router.get("/searchBarcode/:val", auth,
+router.get(
+  "/searchBarcode/:val",
+  auth,
 
-    async (req, res) => {
-        try {
-            const search = req.params.val;
-            const products = await Product.find({
-                $or: [
-                    { 'color.sizes..barcodes..barcode': search },
-                ]
-            });
+  async (req, res) => {
+    try {
+      const search = req.params.val;
+      const products = await Product.find({
+        $or: [{ "color.sizes..barcodes..barcode": search }],
+      });
 
-            res
-                .status(200)
-                .json(products);
-        } catch (err) {
-            console.log(err);
-            res
-                .status(500)
-                .send("Server Error!");
-        }
-    });
-
+      res.status(200).json(products);
+    } catch (err) {
+      console.log(err);
+      res.status(500).send("Server Error!");
+    }
+  }
+);
 
 module.exports = router;
